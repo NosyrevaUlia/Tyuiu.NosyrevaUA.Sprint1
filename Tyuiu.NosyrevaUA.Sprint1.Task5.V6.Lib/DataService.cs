e1a245f2473638b122eb17340e0bb1b3f33516e0
Tyuiu.NosyrevaUA@outlook.com <ulianosyreva752@gmail.com>
2023-09-27 19:09:47 +0500

    Task5

---
+++ Tyuiu.NosyrevaUA.Sprint1.Task5.V6.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.NosyrevaUA.Sprint1.Task5.V6.Lib
+{
+    public class DataService : ISprint1Task5V6
+    {
+        public int Calculate(int k)
+        {
+            if (k%7 == 0)
+            {
+                int n = 7;
+                return n;
+            }
+            else
+            {
+                return k % 7;
+            }        
+
+            
+            
+        }
+    }
+}
---
+++ Tyuiu.NosyrevaUA.Sprint1.Task5.V6.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.NosyrevaUA.Sprint1.Task5.V6.Lib;
+
+namespace Tyuiu.NosyrevaUA.Sprint1.Task5.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int k = 17;
+            Assert.AreEqual(ds.Calculate(k), 3);
+
+            
+        }
+    }
+}
